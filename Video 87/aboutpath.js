4edf6614e5bf90916f332f3c55252e1f3e00f5ba
dhanrajk046 <dhanrajk046@gmail.com>
2026-06-06 22:56:16 +0530

    Added video Lecture 87- File system in nodejs

---
+++ Video 87/aboutpath.js
@@ -0,0 +1,13 @@
+import path from "path"
+
+
+let myPath = "C:\\Users\\pc\\OneDrive\\Desktop\\Sigma web development Course\\Video 87\\harry.txt"
+
+console.log(path.extname(myPath))
+
+console.log(path.dirname(myPath))
+
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/","programs\\harry.txt"))
+
