e02d44e381d8f6266c55cf3c28089b0243194ef5
dhanrajk046 <dhanrajk046@gmail.com>
2026-07-04 20:29:55 +0530

    Video Lecture 102 complete - Exercise 17 solved Card Design

---
+++ Video 102/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
